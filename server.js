90461b3e37a42fe2d32da41d1293e671ba9e2996
MAGS <mathiasgsteenberg@gmail.com>
2023-03-28 14:16:18 +0200

    init

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ con.connect(function(err) {
     });
   });
 
+
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -31,4 +32,27 @@ con.connect(function(err) {
     });
   });
 
+con.connect(function(err) { 
+    if (err) throw err;
+    con.query("DELETE FROM customers WHERE address = 'Highway 37'", function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+  });
+
+  con.connect(function(err) { 
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+
+con.connect(function(err) { 
+    if (err) throw err;
+    con.query("DROP TABLE customers", function (err, result) {
+      if (err) throw err;
+      console.log("Table deleted");
+    });
+  });
 
