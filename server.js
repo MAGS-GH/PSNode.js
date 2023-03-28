bf1158e73feb843f8ffcd2454a6883e5465067a9
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2023-03-28 15:02:49 +0200

    inti

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ con.connect(function(err) {
     });
   });
 
-con.connect(function(err) { 
+/*con.connect(function(err) { 
     if (err) throw err;
     con.query("DELETE FROM customers WHERE address = 'Highway 37'", function (err, result) {
       if (err) throw err;
@@ -55,4 +55,4 @@ con.connect(function(err) {
       console.log("Table deleted");
     });
   });
-
+*/
